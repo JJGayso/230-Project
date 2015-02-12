f685bb287bdf6902210d639de9352049208d06b3
Mercieral <mercieal@rose-hulman.edu>
2015-02-12 14:23:12 -0500

    I don't know what's happening

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 import net.miginfocom.swing.MigLayout;
@@ -14,16 +16,16 @@ public class Main {
 		// TODO Add code here.  Create any other needed methods in this class, 
 		//      and any other classes that you need.
 		JFrame myFrame = new JFrame();
-		myFrame.setLayout(new MigLayout());
+		//myFrame.setLayout(new MigLayout());
 		myFrame.setTitle(" Bits Please ");
 		
-		//ControlPanel controls = new ControlPanel();
+		ControlPanel controls = new ControlPanel();
 		mapPanel map = new mapPanel();
 		infoPanel info = new infoPanel();
 		
-		//myFrame.add(controls, "wrap");
-		myFrame.add(map, "dock west");
-		myFrame.add(info);
+		myFrame.add(controls, BorderLayout.CENTER);
+		myFrame.add(map, BorderLayout.EAST);
+		myFrame.add(info, BorderLayout.SOUTH);
 		
 		myFrame.pack();
 		myFrame.setVisible(true);
