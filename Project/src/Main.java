c1afa3f1f73cda71397db7793592ad990fcf0b30
Mercieral <mercieal@rose-hulman.edu>
2015-02-12 14:13:15 -0500

    useful commit message

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,6 +1,6 @@
-import java.awt.BorderLayout;
-
 import javax.swing.JFrame;
+
+import net.miginfocom.swing.MigLayout;
 
 /**
  * @author Your team number and names here and in all your code files
@@ -14,20 +14,20 @@ public class Main {
 		// TODO Add code here.  Create any other needed methods in this class, 
 		//      and any other classes that you need.
 		JFrame myFrame = new JFrame();
+		myFrame.setLayout(new MigLayout());
 		myFrame.setTitle(" Bits Please ");
 		
-		ControlPanel controls = new ControlPanel();
+		//ControlPanel controls = new ControlPanel();
 		mapPanel map = new mapPanel();
 		infoPanel info = new infoPanel();
 		
 		
-		myFrame.add(controls, BorderLayout.NORTH);
-		myFrame.add(map, BorderLayout.EAST);
-		myFrame.add(info, BorderLayout.SOUTH);
+		//myFrame.add(controls, "wrap");
+		myFrame.add(map, "dock west");
+		myFrame.add(info);
 		
 		
 		
-		myFrame.pack();
 		myFrame.setVisible(true);
 	}
 
