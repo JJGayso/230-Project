69bad14472de0c3cb374c4bfd475385a9b5dbb34
johade777 <patterda@rose-hulman.edu>
2015-02-19 01:24:48 -0500

    Finished search, needs combined with GUI

--- Project/src/SearchMap.java
+++ Project/src/SearchMap.java
@@ -1,20 +1,27 @@
 import java.util.ArrayList;
 
-public class SearchMap{
+public class SearchMap {
 	ArrayList<ArrayList<AmusementPark>> map = new ArrayList<ArrayList<AmusementPark>>();
-	
-	public SearchMap(){
-		
+
+	public SearchMap() {
+		for (int i = 0; i < 20; i++) {
+			map.add(i, null);
+		}
 	}
-	
-	public void insertPark(AmusementPark park){
+
+	public void insertPark(AmusementPark park) {
 		Integer temp = park.getLevelOfFun();
-		ArrayList<AmusementPark> list = map.get(temp);
-		
-		list.add(park);
+		if (map.get(temp) == null) {
+			ArrayList<AmusementPark> adding = new ArrayList<AmusementPark>();
+			adding.add(park);
+			map.set(temp, adding);
+		} else {
+			ArrayList<AmusementPark> list = map.get(temp);
+			list.add(park);
+		}
 	}
-	
-	public ArrayList<AmusementPark> getList(Integer level){
+
+	public ArrayList<AmusementPark> getList(Integer level) {
 		return map.get(level);
 	}
 }
